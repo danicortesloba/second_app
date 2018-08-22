3c720901488c5ce4b68325e8c60cf9aed25f3194
Dani <dustyglass@gmail.com>
2018-08-22 20:57:22 -0300

    ADD: navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/landing'
+
   resources :students
   root 'students#index'
   # The priority is based upon order of creation: first created -> highest priority.
